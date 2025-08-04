289971cd68bf1c0294ac20357968812cc9b81652
Reprintx <superball41187@gmail.com>
2025-08-04 11:09:45 +0700

    init + recovery
    Github หลักโดน Flaged ย้ายมาอันนี้ครับ

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Header,Content,Footer } from "./func-components";
+import { EventData } from "./event-data";
+
+function App() {
+  return <EventData/>
+}
+
+export default App;
