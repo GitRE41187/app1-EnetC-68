f0d5adabf7535dbd5c8c476b0133bd95a0e7d9fc
Reprintx <superball41187@gmail.com>
2025-08-25 10:29:59 +0700

    Context

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
-import { Header,Content,Footer } from "./func-components";
-import { EventData } from "./event-data";
+import { userContext } from './context'
+import Header2 from './context-header2'
+import Content2 from './context-content2'
 
-function App() {
-  return <EventData/>
+export default function App(){
+  let [user, setUser] = React.useState();
+  return(
+      <userContext.Provider value={[user, setUser]}>
+        <Header2/>
+        <Content2/>
+      </userContext.Provider>
+  )
 }
-
-export default App;
